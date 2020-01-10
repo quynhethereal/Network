49b0880e753aaa7b86b99953beb7facdc9a188cd
thanhlongb <s3748575@rmit.edu.vn>
2020-01-10 11:27:16 +0700

    update

--- src/DemoClient.java
+++ src/DemoClient.java
@@ -2,7 +2,7 @@ public class DemoClient {
 
     public static void main(String[] args) throws Exception {
         final int PORT_NUMBER = 8888;
-        final String HOST = "localhost";
+        final String HOST = "157.230.37.0";
 
         Client client = new Client(HOST, PORT_NUMBER);
         client.start();
